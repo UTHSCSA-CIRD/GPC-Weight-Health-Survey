16f5c5df42667a6a37a2b6c3f28627ca14d9ec81
Alex F. Bokov <bokov@uthscsa.edu>
2016-04-29 11:58:03 -0500

    This concludes a hack-session during which ObesityScript.R was refactored for conciseness and generality. Moved the plotting and trees and stuff to new file called DataExploration.R. During this session, ciRd.R dependency was introduced into ObesityScript.R, but we plan to soon merge it with obesitySurveyHelpers.R. Also, now three data.frames get saved: obd, obd.backup, and samp

--- Obesity Survey/DataExploration.R
+++ Obesity Survey/DataExploration.R
@@ -1,5 +1,6 @@
+
 source('obesitySurveyHelpers.R');
-#Some plots by race
+#'Some plots by race
 runByRaceVariable(samp, "possible_research", "Interested in Being Contacted for Research")
 runByRaceVariable(samp, "survey_contact_method", "Survey Contact Method")
 runByRaceVariable(samp, "cancer_anytype_self", "Cancer - Self")
@@ -10,15 +11,15 @@ runByRaceVariable(samp, "latino_origin", "Latino Origin")
 runByRaceVariable(samp, "income", "Income")
 runByRaceVariable(samp, "insurance", "Insurance by Race")
 runGGPLOT(samp, "income", "insurance", xlab ="Income", ylab = "Insurance", omitNA_X = FALSE)
-#lets play with some mosaic plots...
+#'lets play with some mosaic plots...
 ggMosaicP(samp$site, samp$possible_research)
 mosaic(structable(site ~ surv_2, data = samp), shade = TRUE, legend = TRUE)
 
-#categorical trees-- just playing with these for now. 
-#This was the most interesting one I found
+#'categorical trees-- just playing with these for now. 
+#'This was the most interesting one I found
 library("party")
 fit <- ctree(income ~ site + Race, data = samp)
-#Not as much a fan of this one, maybe Alex can work out what to do with it. 
+#'Not as much a fan of this one, maybe Alex can work out what to do with it. 
 library("rpart")
 fit <- rpart(income ~ site + Race, method = "class", data = samp)
 printcp(fit)
@@ -27,7 +28,7 @@ summary(fit)
 plot(fit, uniform=TRUE)
 text(fit, use.n=TRUE, all=TRUE, cex=.8)
 
-#Plots by willingness to participate - most of the factors don't seem to stand out
+#'Plots by willingness to participate - most of the factors don't seem to stand out
 runByWilling2P(samp, "site")
 runByWilling2P(samp, "income", "Income by Willingness to Participate")
 runByWilling2P(samp, "survey_contact_method") #People have a hard time saying no on the phone? 
