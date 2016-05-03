09526ff233cb9f4fe3aea64a886133d00ba59b6f
Laura <Manuells@uthscsa.edu>
2016-05-03 15:03:09 -0500

    Moved ciRd.R for the server to access, added constellation, box plot and violin to the app as separate tabs.

--- Obesity Survey/server.R
+++ Obesity Survey/server.R
@@ -2,7 +2,10 @@ library(shinyBS)
 library(shiny)
 library(ggplot2)
 library(shinyjs)
+library(e1071);
+library(psy);
 source("obesitySurveyHelpers.R")
+source("ciRd.R")
 
 shinyServer(
   function(input, output, session){
@@ -11,13 +14,14 @@ shinyServer(
         load("survSave.rdata")
     dataDic = lapply(samp, class)
     valsFactor = names(dataDic[dataDic == "factor"])
+    valsNumeric = names(dataDic[dataDic == "numeric" | dataDic == "integer" ])
+    valsNonText = c(valsFactor, valsNumeric)
     #session$sendCustomMessage(type = "bsAlertClose", "gError")
     
     output$graphSidePanel <- renderUI({
-      flowLayout(
+      fluidRow(
         p("Currently only barplots are available, please make your selections."),
         selectInput("xVal", "X Value", valsFactor ),
-        
         checkboxInput("xOmit", "Omit blanks?", value = TRUE),
         selectInput("plotFill", "Fill Value", valsFactor),
         checkboxInput("fillOmit", "Omit blanks?", value = FALSE),
@@ -42,6 +46,87 @@ shinyServer(
       )
       out <- table(samp[,c(input$xVal,input$plotFill)]);
       if(input$xVal==input$plotFill) out else addmargins(out);
-    })
+    })# END OUTPUT freqTable
+    output$consetllationSide <- renderUI({
+      if(input$focusedPCA){
+        return({verticalLayout(
+          hr(),
+          p("The closer a point is to the center, the more closely the column it 
+            represents is correlated with the response variable in the center. Green 
+            indicates positive correlations and yellow, inverse correlations.
+            the response variable."),
+          hr(),
+          selectInput("constResponseVar","Response Variable", valsNonText)
+        )})
+      }else{
+        return({
+          verticalLayout(
+            hr(),
+            p("Each point is a column from the dataset. The closer they are together the 
+            stronger their positive correlation, and the closer they are to 180 degrees,
+            the stronger their negative correlation. Variables 90 degrees to each other are 
+            uncorrelated (independent)."),
+            hr(),
+            p("Use these sliders to rotate the points until they become easy to see."),
+            sliderInput('constVSlider', "Y-Axis", min = 0, max = 180, value = 1, step = 5, round = 0),
+            sliderInput('constHSlider', "X-Axis", min = 0, max = 180, value = 1, step = 5, round = 0),
+            sliderInput('constFSlider', "Z-Axis", min = 0, max = 180, value = 1, step = 5, round = 0)
+          )
+        })
+      }#end else not focused PCA
+    })#END OUTPUT constellationSide 
+    output$constellationPlot <- renderPlot({
+      if(input$focusedPCA){
+        validate(
+          need(input$constResponseVar, 'Response variable must be selected for a focused PCA plot')
+        )
+        if(input$constSurv2RespOnly){
+          pcawrap(subset(samp,s2resp=='Yes'), respvar = input$constResponseVar, pca='f'                       
+                  ,contraction='Yes')
+        }else{
+          pcawrap(samp, respvar = input$constResponseVar,pca='f', contraction='Yes')
+        }
+      }else{
+        validate(
+          need(input$constVSlider, 'Warning slider value missing'),
+          need(input$constHSlider, 'Warning slider value missing'),
+          need(input$constFSlider, 'Warning slider value missing')
+        )
+        if(input$constSurv2RespOnly){
+          pcawrap(subset(samp,s2resp=='Yes'), nbsphere=1, back=T,v=input$constVSlider,
+                  h=input$constHSlider,f=input$constFSlider)
+        }else{
+          pcawrap(samp, nbsphere=1, back=T,v=input$constVSlider,
+                  h=input$constHSlider,f=input$constFSlider)
+        }
+      }
+    })#END PLOT constellationPlot
+    output$boxPlotSide <- renderUI({
+      verticalLayout(
+        selectInput("boxPlotX","Discrete Variable(X)", valsFactor),
+        selectInput("boxPlotY", "Continuous Variable(Y)", valsNumeric)
+    )})# END UI for boxPlot Sidebar
+    output$boxPlot <- renderPlot({
+      validate(
+        need(input$boxPlotX, 'Warning X value for boxplot missing.'),
+        need(input$boxPlotY, 'Warning Y value for boxplot missing.')
+      )
+      ggplot(samp, aes_string(input$boxPlotX, input$boxPlotY)) +
+          geom_boxplot()
+    })#End plot for boxPlot
+    output$violinPlotSide <- renderUI({
+      verticalLayout(
+        selectInput("violinPlotX","Discrete Variable(X)", valsFactor),
+        selectInput("violinPlotY", "Continuous Variable(Y)", valsNumeric)
+      )})# END UI for boxPlot Sidebar
+    output$violinPlot <- renderPlot({
+      validate(
+        need(input$violinPlotX, 'Warning X value for violin plot missing.'),
+        need(input$violinPlotY, 'Warning Y value for violin plot missing.')
+      )
+      ggplot(samp, aes_string(input$violinPlotX, input$violinPlotY)) +
+        geom_violin()
+    })#End plot for boxPlot
+    
   }
 )
--- Obesity Survey/ui.R
+++ Obesity Survey/ui.R
@@ -1,17 +1,46 @@
 shinyUI(
   fluidPage(
     titlePanel("Obesity Survey Sample Data Review"),
-    sidebarLayout(
-      sidebarPanel(
-        #Graph options
-        uiOutput("graphSidePanel")
-      ), #end sidebar panel
+    tabsetPanel(
+      tabPanel("Bar Graph", #tabpanel for the bargraph
+        sidebarLayout(#BARGRAPH
+          sidebarPanel(
+            #Graph options
+            uiOutput("graphSidePanel")
+          ), #end bargraph sidebar panel
+          mainPanel(
+    #        bsAlert("graphError"),
+            plotOutput("visPlot"),
+            tableOutput("freqTable")
+          )#end bargraph mainPanel
+        )),#end sidebarLayout/TabPanel BARGRAPH
+      tabPanel("Constellation",sidebarLayout(
+        sidebarPanel(fluidRow( # CONSTELLATIONS
+            checkboxInput("constSurv2RespOnly","Only Survey 2 Respondants?"),
+            checkboxInput("focusedPCA", "Focused PCA Plot?"),
+            uiOutput("consetllationSide")
+        )),#end constellation sideBar
+        mainPanel(
+          plotOutput("constellationPlot")
+        )#end constellation mainpanel
+      )),#End sidbarLayout/TabPanel CONSTELLATIONS
+    tabPanel("Box Plot",sidebarLayout(
+      sidebarPanel( # BoxPlot
+        uiOutput("boxPlotSide")
+      ),#end boxPlot sideBar
       mainPanel(
-#        bsAlert("graphError"),
-        plotOutput("visPlot"),
-        tableOutput("freqTable")
-      )#end mainPanel
-    )#end sidebarLayout
+        plotOutput("boxPlot")
+      )#end BoxPlot mainpanel
+    )),#End sidbarLayout/TabPanel BOXPLOTTAB
+    tabPanel("Violin",sidebarLayout(
+      sidebarPanel( # Violin
+        uiOutput("violinPlotSide")
+      ),#end violinPlot sideBar
+      mainPanel(
+        plotOutput("violinPlot")
+      )#end violinPlot mainpanel
+    ))#End sidbarLayout/TabPanel VIOLINPLOTTAB
+    )#end tabsetPanel
   )#end fluidPage
   #
 )
