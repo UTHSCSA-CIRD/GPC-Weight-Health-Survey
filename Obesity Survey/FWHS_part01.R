f740df1fe5ec1e868b84b59bd30280ff479e3a32
Alex F. Bokov <bokov@uthscsa.edu>
2016-05-03 23:44:46 -0500

    Autocommit

--- Obesity Survey/FWHS_part01.R
+++ Obesity Survey/FWHS_part01.R
@@ -12,7 +12,7 @@ if(!all(c(require(party),require(rpart),require(psy)))){
   install.packages(c('party','rpart','psy'));
 }
 load('survSave.rdata');
-resp <- 'research_feeling';
+resp <- 'deid_data';
 preds <- names(samp);
 factors <- vs(samp,'f');
 responded <- subset(samp,s1s2resp=='Yes');
