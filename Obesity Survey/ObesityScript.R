d1ecc6003744adc602aa92346237a6153787a4e0
Alex F. Bokov <bokov@uthscsa.edu>
2016-08-12 20:14:13 -0500

    Fixed a couple of bugs

--- Obesity Survey/ObesityScript.R
+++ Obesity Survey/ObesityScript.R
@@ -2,6 +2,7 @@
 library(plyr)
 library(reshape)
 library(vcd)
+library(digest)
 
 # some handy functions
 source('obesitySurveyHelpers.R');
@@ -119,7 +120,7 @@ filter_kids = subset(samp,pat_age < 18)
 serverData = list(samp, filter_surv2, filter_surv2_kids, filter_kids)
 serverDataDic = c("No filter", "Survey 2 Respondants Only", "Survey 2 Respondants & Pat < 18", "Pat < 18")
 serverTitle = "Obesity Survey Sample Data Review."
-serverStatement = quote(h4("Enter valid shiny tags and information here. Mostly... you know... Like a link to a data dictionary or something.")))
+serverStatement = quote(h4("Enter valid shiny tags and information here. Mostly... you know... Like a link to a data dictionary or something."))
 #DEBUG#save(samp, file = "survSavet0.rdata")
 #DEBUG#save(serverData, file = "survSavet1.rdata")
 #DEBUG#save(serverData ,serverDataDic , file="survSavet2.rdata")
