e3773529ce882bfa7a7bd8da0f87eacb8f74a8e2
Alex F. Bokov <bokov@uthscsa.edu>
2017-01-15 21:28:58 -0600

    More tweaks. Okay, now I'm done

--- Obesity Survey/ObesityScript.R
+++ Obesity Survey/ObesityScript.R
@@ -1,7 +1,7 @@
-library(plyr)
-library(reshape)
-library(vcd)
-library(digest)
+library(plyr);
+library(reshape);
+library(vcd);
+library(digest);
 
 # some handy functions
 source('obesitySurveyHelpers.R');
@@ -10,7 +10,7 @@ rseed <- 6062016;
 set.seed(rseed);
 
 #load clean and save 
-obd <- read.table("testoutput.csv", header = TRUE, sep = "\t")
+obd <- read.table("testoutput.csv", header = TRUE, sep = "\t");
 
 # set variables all in one place if practical
 textfields <- grep('^other_|ans6_response$|types2_child$',names(obd),v=T);
@@ -132,7 +132,7 @@ obd[,c('height_req','height_feet','height_value_cm'
        ,'weight_req','weight_value_kg')] <- NULL;
 
 samp <- pickSample(obd, .25);
-save(obd,rseed,obd.backup,samp, file = "survProcessed.rdata")
+save(obd,rseed,obd.backup,samp, file = "survProcessed.rdata");
 # We delete the ID-type variables
 samp <- samp[,setdiff(names(samp),c(toOmit,textfields))];
 samp <- samp[,c(vs(samp,'f'),vs(samp))];
@@ -140,12 +140,10 @@ serverHash <- digest("ChangeThisInYourCode!", algo = "sha512", ascii = TRUE);
 filter_surv2 <- subset(samp,s2resp=='Yes'|surv_2=='TRUE');
 # BUG: in UTHSCSA, s2resp is all NA... but surv_2 seems right...
 filter_surv2_kids <- subset(samp,(s2resp=='Yes'|surv_2=='TRUE') & pat_age < 18);
-filter_kids <- subset(samp,pat_age < 18)
-serverData = list(samp, filter_surv2, filter_surv2_kids, filter_kids)
-serverDataDic = c("No filter", "Survey 2 Respondants Only", "Survey 2 Respondants & Pat < 18", "Pat < 18")
-serverTitle = "Obesity Survey Sample Data Review."
-serverStatement = quote(h4("Enter valid shiny tags and information here. Mostly... you know... Like a link to a data dictionary or something."))
-#DEBUG#save(samp, file = "survSavet0.rdata")
-#DEBUG#save(serverData, file = "survSavet1.rdata")
-#DEBUG#save(serverData ,serverDataDic , file="survSavet2.rdata")
-save(serverData ,serverDataDic , serverHash, serverTitle, serverStatement,file="survSave.rdata")
+filter_kids <- subset(samp,pat_age < 18);
+serverData <- list(samp, filter_surv2, filter_surv2_kids, filter_kids);
+serverDataDic <- c("No filter", "Survey 2 Respondants Only", "Survey 2 Respondants & Pat < 18", "Pat < 18");
+serverTitle <- "Obesity Survey Sample Data Review.";
+serverStatement <- quote(h4("Enter valid shiny tags and information here. Mostly... you know... Like a link to a data dictionary or something."));
+save(serverData ,serverDataDic , serverHash, serverTitle, serverStatement,file="survSave.rdata");
+
