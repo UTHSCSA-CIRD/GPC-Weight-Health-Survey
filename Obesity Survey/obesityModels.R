4d74e61e87834156b9726920f6aa9cb602d6c5b4
Alex F. Bokov <bokov@uthscsa.edu>
2018-04-04 15:16:19 -0500

    GLMM fits will be done in the new obesityModels.R script.

---
+++ Obesity Survey/obesityModels.R
@@ -0,0 +1,32 @@
+#' ---
+#' title: "Obesity Model Fitting"
+#' author: "Alex F. Bokov"
+#' date: "April 4th, 2018"
+#' ---
+#' 
+#+ include=FALSE,cache=FALSE,echo=FALSE
+#require(xtable);
+require(magrittr); require(dplyr); require(knitr);
+require(lme4);
+#require(tableone); require(broom); require(dummies); require(readr);
+require(pander);
+#knitr::opts_chunk$set(echo = TRUE);
+datafile<-'obesityPaper01.rdata';
+#datadict<-'data_dictionary.tsv';
+#noticefile<-'coauthors_notice.md';
+#dir<-'/tmp/gpcob/GPC-Weight-Health-Survey/Obesity Survey/';
+options(knitr.kable.NA='-');
+#setwd(dir);
+source('functions.R');
+source('trailR.R');
+currentscript <- parent.frame(2)$ofile;
+if(is.null(currentscript)) currentscript <- 'RUN_FROM_INTERACTIVE_SESSION';
+tself(currentscript,production=T);
+tload(datafile);
+
+#' Our basic, additive GLMM model with site as random effect
+glmer_s1s2 <- glmer(s1s2resp ~ ses_hispanic + pat_sex + pat_age + pat_bmi_raw +
+                      ses_income + ses_race + ses_finclass + (1|site)
+                    ,family=binomial(link='logit'),data=df_unilogist);
+#' Then it gets real slow...
+#glmer_s1s2_2 <- update(glmer,.~(.)^2);
