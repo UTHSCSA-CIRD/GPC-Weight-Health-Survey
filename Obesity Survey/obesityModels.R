392ee2b1b73c6d38498474987d7ae8be8ee4f83c
Alex F. Bokov <bokov@uthscsa.edu>
2018-04-04 19:30:03 -0500

    More notes in obesityModels.R on possibilities for doing variable selection on a mixed effect logistic model. Didn't realize this isn't more of a solved problem

--- Obesity Survey/obesityModels.R
+++ Obesity Survey/obesityModels.R
@@ -30,3 +30,17 @@ glmer_s1s2 <- glmer(s1s2resp ~ ses_hispanic + pat_sex + pat_age + pat_bmi_raw
                     + (1|site),family=binomial(link='logit'),data=df_unilogist);
 #' Then it gets real slow...
 #glmer_s1s2_2 <- update(glmer,.~(.)^2);
+#' One issue is missing data-- all of Iowa's incomes are missing. We need to 
+#' either infer them or drop Iowa from this type of analysis.
+# foo<-df_unilogist[rownames(df_unilogist) %in% rownames(na.omit(df_unilogist[,c('pat_sex','pat_age','pat_bmi_raw','ses_income','ses_race','ses_finclass')])),] %>% droplevels
+#' The above at least gets rid of that issue. Also, though step, stepAIC, and 
+#' lmerTest do not work with glmer, drop1 does.
+#' 
+#' Tried glmmLasso, and it fit something, but no standard
+#' errors nor z.values, no idea if I'm using it correctly
+#' 
+#' I guess I just need to find a fast computer that I won't
+#' need for a while, fit a ~(.)^2 formula, and let it grind, 
+#' then come back and repeatedly do drop1() on it...
+#' # 
+ 
